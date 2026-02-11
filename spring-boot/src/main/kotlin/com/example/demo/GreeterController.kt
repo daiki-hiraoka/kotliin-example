5c968b72dd39bba731a0a660081fa7b9df0d8960
daiki-hiraoka <dai.0120.ki@gmail.com>
2026-02-11 21:22:46 +0900

    Postメソッドでbodyからパラメータを受け取るAPI追加

--- spring-boot/src/main/kotlin/com/example/demo/GreeterController.kt
+++ spring-boot/src/main/kotlin/com/example/demo/GreeterController.kt
@@ -2,11 +2,14 @@ package com.example.demo
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 data class HelloResponse(val message: String)
+data class HelloRequest(val name: String)
 
 // RestControllerアノテーションをつけると戻り値のオブジェクトをJSONにシリアライズしてレスポンスとして返す
 // @ControllerのままだとSpring Bootが戻り値をビュー名だと認識して、Thymeleafを通じて対応するHTMLを探しに行ってしまうのでエラーになる
@@ -21,5 +24,10 @@ class GreeterController {
     @GetMapping("/hello/{name}")
     fun helloPathValue(@PathVariable("name") name: String): HelloResponse {
         return HelloResponse("Hello $name")
+    }
+
+    @PostMapping("/hello")
+    fun helloByPost(@RequestBody request: HelloRequest): HelloResponse {
+        return HelloResponse("Hello ${request.name}")
     }
 }
