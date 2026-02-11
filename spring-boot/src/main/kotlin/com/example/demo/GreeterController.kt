7726cccd39f2f43b2d3f31b5a478fc5e8ad1e49f
daiki-hiraoka <dai.0120.ki@gmail.com>
2026-02-11 21:13:00 +0900

    パスからパラメータを受け取るAPIを追加

--- spring-boot/src/main/kotlin/com/example/demo/GreeterController.kt
+++ spring-boot/src/main/kotlin/com/example/demo/GreeterController.kt
@@ -1,6 +1,7 @@
 package com.example.demo
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -14,6 +15,11 @@ data class HelloResponse(val message: String)
 class GreeterController {
     @GetMapping("/hello")
     fun hello(@RequestParam("name") name: String): HelloResponse {
-        return HelloResponse("Hello ${name}")
+        return HelloResponse("Hello $name")
+    }
+
+    @GetMapping("/hello/{name}")
+    fun helloPathValue(@PathVariable("name") name: String): HelloResponse {
+        return HelloResponse("Hello $name")
     }
 }
