f034ea7b246fc040938bec96683d7b7068329999
daiki-hiraoka <dai.0120.ki@gmail.com>
2026-02-11 10:55:05 +0900

    Spring Bootのプロジェクトを追加

--- first-kotlin/settings.gradle.kts
+++ first-kotlin/settings.gradle.kts
@@ -7,4 +7,6 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "KotlinExamply"
-
+include("untitled")
+include("first-kotlin")
+include("spring-boot")
