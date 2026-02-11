9f01dcd3055602c042d5b6b6a8c1e06023126d6a
daiki-hiraoka <dai.0120.ki@gmail.com>
2026-02-11 20:42:13 +0900

    gradleの設定のpluginとdependenciesについてコメントを残す

--- spring-boot/build.gradle.kts
+++ spring-boot/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
+    // kotlin()のプラグインはGradleでのKotlinプロジェクトのビルド、KotlinでのSpring Bootの使用で必要となる
     kotlin("jvm")
     kotlin("plugin.spring")
+    // Spring BootアプリケーションをGradleから実行するためのもの。bootRunという起動タスクが提供されている
     id("org.springframework.boot")
+    // 依存関係の管理のサポートをしてくれるプラグイン
+    // Spring Bootの起動に必要なstarter関連の依存関係を追加するとき、入れているSpring Bootで必要となるバージョンを指定してくれる
     id("io.spring.dependency-management")
 }
 
@@ -20,9 +24,14 @@ repositories {
 }
 
 dependencies {
+    // Thymeleafというテンプレートエンジンを使用するためのstarter
+    // ThymeleafでHTMLなどのwebページを作成する
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    // ルーティングなどのwebアプリケーションのサーバーサイドプログラムで必要な機能を提供するstarter
+    // Spring MVC, Jackson, Tomcat（組み込みサーバー）などが自動で追加される
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // JacksonとはJSONのシリアライズ、デシリアライズをするライブラリ
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
