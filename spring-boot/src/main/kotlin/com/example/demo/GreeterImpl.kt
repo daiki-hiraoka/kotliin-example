4b22daa822ec33e151da0bb42e89d6d4eab778d8
daiki-hiraoka <dai.0120.ki@gmail.com>
2026-02-11 21:25:29 +0900

    Greeterクラスをインターフェースを使って実装

---
+++ spring-boot/src/main/kotlin/com/example/demo/GreeterImpl.kt
@@ -0,0 +1,10 @@
+package com.example.demo
+
+import org.springframework.stereotype.Component
+
+@Component
+class GreeterImpl: Greeter {
+    override fun sayHello(name: String): String {
+        return "Hello $name"
+    }
+}
