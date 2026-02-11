f994134ae8fbaad2bf645dca3d46b8dfd6f263b8
daiki-hiraoka <dai.0120.ki@gmail.com>
2026-02-11 21:10:02 +0900

    JSONで返すGreeterControllerを作成

---
+++ spring-boot/src/main/kotlin/com/example/demo/GreeterController.kt
@@ -0,0 +1,19 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+data class HelloResponse(val message: String)
+
+// RestControllerアノテーションをつけると戻り値のオブジェクトをJSONにシリアライズしてレスポンスとして返す
+// @ControllerのままだとSpring Bootが戻り値をビュー名だと認識して、Thymeleafを通じて対応するHTMLを探しに行ってしまうのでエラーになる
+@RestController
+@RequestMapping("greeter")
+class GreeterController {
+    @GetMapping("/hello")
+    fun hello(@RequestParam("name") name: String): HelloResponse {
+        return HelloResponse("Hello ${name}")
+    }
+}
