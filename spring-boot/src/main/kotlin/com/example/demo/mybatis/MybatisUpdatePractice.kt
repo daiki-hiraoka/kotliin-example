5810ea5a1bb84b1476f83b7ac8b0226ec2bb4f96
daiki-hiraoka <dai.0120.ki@gmail.com>
2026-02-14 20:45:13 +0900

    whereでupdateするレコードを選択

--- spring-boot/src/main/kotlin/com/example/demo/mybatis/MybatisUpdatePractice.kt
+++ spring-boot/src/main/kotlin/com/example/demo/mybatis/MybatisUpdatePractice.kt
@@ -1,11 +1,16 @@
 package com.example.demo.mybatis
 
+import database.UserDynamicSqlSupport.User.name
 import database.UserRecord
+import database.update
 import database.updateByPrimaryKeySelective
+import database.updateSelectiveColumns
+import org.mybatis.dynamic.sql.SqlBuilder.isEqualTo
 
 fun main() {
     val mybatisUpdatePractice = MybatisUpdatePractice()
     mybatisUpdatePractice.update()
+    mybatisUpdatePractice.update2()
 }
 
 class MybatisUpdatePractice {
@@ -14,6 +19,19 @@ class MybatisUpdatePractice {
         MybatisSessionFactoryImpl().withSession { session ->
             val mapper = session.getMapper(database.UserMapper::class.java)
             val count = mapper.updateByPrimaryKeySelective(user)
+            session.commit()
+            println("${count}行のレコードを更新しました")
+        }
+    }
+
+    fun update2() {
+        val user = UserRecord(profile = "Good Morning")
+        MybatisSessionFactoryImpl().withSession { session ->
+            val mapper = session.getMapper(database.UserMapper::class.java)
+            val count = mapper.update {
+                updateSelectiveColumns(user)
+                where(name, isEqualTo("Shiro"))
+            }
             session.commit()
             println("${count}行のレコードを更新しました")
         }
