fcf1f0688940efe2e2882fb33f28a11d362aaa6c
daiki-hiraoka <dai.0120.ki@gmail.com>
2026-02-14 20:27:36 +0900

    insertMultipleを使ってみる

--- spring-boot/src/main/kotlin/com/example/demo/mybatis/MybatisInsertPractice.kt
+++ spring-boot/src/main/kotlin/com/example/demo/mybatis/MybatisInsertPractice.kt
@@ -3,10 +3,12 @@ package com.example.demo.mybatis
 import database.UserMapper
 import database.UserRecord
 import database.insert
+import database.insertMultiple
 
 fun main() {
     val mybatisInsertPractice = MybatisInsertPractice(MybatisSessionFactoryImpl())
-    mybatisInsertPractice.insert()
+//    mybatisInsertPractice.insert()
+    mybatisInsertPractice.insertRecords()
 }
 
 class MybatisInsertPractice(
@@ -17,6 +19,16 @@ class MybatisInsertPractice(
         mybatisSessionFactory.withSession { session ->
             val mapper = session.getMapper(UserMapper::class.java)
             val count = mapper.insert(user)
+            session.commit()
+            println("${count}行のレコードを挿入しました")
+        }
+    }
+
+    fun insertRecords() {
+        val userList = listOf(UserRecord(104, "Goro", 22, "Hi"), UserRecord(105, "Rokuro", 28, "Hey"))
+        mybatisSessionFactory.withSession { session ->
+            val mapper = session.getMapper(UserMapper::class.java)
+            val count = mapper.insertMultiple(userList)
             session.commit()
             println("${count}行のレコードを挿入しました")
         }
